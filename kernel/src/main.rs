57dc0c89956305b300747da6f75a656fbdaec474
German <germangolozubow@gmail.com>
2024-10-08 20:16:07 +0300

    docs and physical memory mapping added

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,6 +1,8 @@
 #![no_std]
 #![no_main]
 
+use bootloader_api::config::Mapping;
+
 mod com_ports;
 mod serial_debug;
 
@@ -10,8 +12,9 @@ static BOOTLOADER_CONFIG: bootloader_api::BootloaderConfig = {
 
     // Configure mappings created by bootloader
     let mut mappings = bootloader_api::config::Mappings::new_default();
-    // Higher half
-    mappings.dynamic_range_start = Some(0xFFFF_8000_0000_0000);
+    // Complete physical memory mapping with offset
+    // ../doc/virtual_memory_layout.txt
+    mappings.physical_memory = Some(Mapping::FixedAddress(0xFFFF_9000_0000_0000));
 
     config.mappings = mappings;
 
